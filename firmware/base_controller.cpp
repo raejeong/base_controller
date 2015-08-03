be13d0cfe7a1fffa72474ce751b81e476293d90b
Brett Hagman <bhagman@roguerobotics.com>
2015-08-03 17:34:41 -0400

    Updated base_controller.cpp to use new WireData library.  UNTESTED.

--- firmware/base_controller.cpp
+++ firmware/base_controller.cpp
@@ -5,10 +5,34 @@
  * Email  :: raychanjeongjeong@gmail.com
  */
 #include <Wire.h>
-#include <I2C_Anything.h>
+#include <WireData.h>
 #include <ros.h>
 #include <std_msgs/Float32.h>
 
+/*
+ * I2C Commands available on each joint controller.
+ *
+ */
+
+#define I2C_COMMAND_NULL                        0
+#define I2C_COMMAND_JOINT_SET_SETPOINT          1
+#define I2C_COMMAND_JOINT_GET_SETPOINT          2
+#define I2C_COMMAND_JOINT_GET_POSITION          3
+#define I2C_COMMAND_JOINT_SET_KP                4
+#define I2C_COMMAND_JOINT_GET_KP                5
+#define I2C_COMMAND_JOINT_GET_CALIBRATION_STATE 6
+#define I2C_COMMAND_JOINT_GET_DIRECTION         7
+#define I2C_COMMAND_JOINT_SET_DIRECTION         8
+#define I2C_COMMAND_JOINT_GET_CAL_DIRECTION     9
+#define I2C_COMMAND_JOINT_SET_CAL_DIRECTION     10
+#define I2C_COMMAND_JOINT_CALIBRATE             11
+#define I2C_COMMAND_JOINT_HALT                  12
+#define I2C_COMMAND_JOINT_HOME                  13
+#define I2C_COMMAND_JOINT_MOTOR_OFF             14
+
+// 7 bit I2C/TWI addresses are in the range of 0x08 to 0x77
+
+const int addressJoint1 = 0x08;
 
 /*
  * Function prototypes
@@ -17,17 +41,18 @@ void positionCb(const std_msgs::Float32& position_msg);
 
 
 /*
- * Variable to store the new position
+ * Variable to store the current and new setpoint
  */
-float new_position; 
+float current_setpoint;
+float new_setpoint;
 
 
 /*
  * ROS node handle, encoder reading, subscriber and publisher
  */
-std_msgs::Float32 enc_reading_msg;
-ros::Subscriber<std_msgs::Float32> position_listener("position", &positionCb);
-ros::Publisher pub_enc("enc", &enc_reading_msg);
+std_msgs::Float32 joint_position_msg;
+ros::Subscriber<std_msgs::Float32> setpoint_listener("position", &setpointCb);
+ros::Publisher pub_position("enc", &joint_position_msg);
 ros::NodeHandle nh;
 
 
@@ -36,25 +61,40 @@ void setup()
   Wire.begin();
 
   nh.initNode();
-  nh.subscribe(position_listener);
-  nh.advertise(pub_enc);
+  nh.subscribe(setpoint_listener);
+  nh.advertise(pub_position);
 
-  new_position  = 0.0; // Default position
+  current_setpoint  = 0.0; // Default setpoint
+  new_setpoint = 0.0;
 }
 
 
 void loop()
 {
-  Wire.requestFrom(1,3);
-  int enc_reading = Wire.read();
+  double current_position;
 
-  enc_reading_msg.data = (double)enc_reading;
-  pub_enc.publish(&enc_reading_msg);
-  
-  double new_set_point = new_position;
-  Wire.beginTransmission(1);
-  I2C_writeAnything(new_set_point);
+  // Read from the joint's position register.
+  Wire.beginTransmission(addressJoint1);
+  Wire.write(I2C_COMMAND_JOINT_GET_POSITION);
   Wire.endTransmission();
+  
+  Wire.requestFrom(addressJoint1, sizeof current_position));
+
+  wireReadData(current_position);
+
+  // Report it to the publisher.
+  joint_position_msg.data = current_position;
+  pub_position.publish(&joint_position_msg);
+
+  // Now, check if we have a new setpoint from the listener.
+  if (new_setpoint != current_setpoint)
+  {
+    Wire.beginTransmission(addressJoint1);
+    Wire.write(I2C_COMMAND_JOINT_SET_SETPOINT);
+    wireWriteData(new_setpoint);
+    Wire.endTransmission();
+    current_setpoint = new_setpoint;
+  }
 
   nh.spinOnce();
 
@@ -63,10 +103,12 @@ void loop()
 
 
 /*
- * Called when there is new msg on the position topic. Updates the position
- * value with the new position msg value
+ * Called when there is new msg on the setpoint topic. Updates the setpoint
+ * value with the new setpoint msg value
  */
-void positionCb(const std_msgs::Float32& position_msg)
+void setpointCb(const std_msgs::Float32& setpoint_msg)
 {
-  new_position = position_msg.data;
+  // Copy payload from subscriber to our joint 
+  new_setpoint = setpoint_msg.data;
 }
+
